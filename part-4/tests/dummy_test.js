8f964ecf07549b5f887ef752fd6488cf598ed304
harinda05 <harindaburndown@gmail.com>
2025-01-04 15:01:32 +0200

    4.5 - favoriteBlog test

--- part-4/tests/dummy_test.js
+++ part-4/tests/dummy_test.js
@@ -2,6 +2,31 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
+const sample_blogs_3 = [{
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0
+  }]
+
 test('dummy returns one', () => {
   const blogs = []
 
@@ -18,32 +43,7 @@ describe('totalLikes', () => {
     });
 
     test('total likes for multiple blogs', () => {
-        const blogs = [
-                {
-                  _id: "5a422a851b54a676234d17f7",
-                  title: "React patterns",
-                  author: "Michael Chan",
-                  url: "https://reactpatterns.com/",
-                  likes: 7,
-                  __v: 0
-                },
-                {
-                  _id: "5a422aa71b54a676234d17f8",
-                  title: "Go To Statement Considered Harmful",
-                  author: "Edsger W. Dijkstra",
-                  url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-                  likes: 5,
-                  __v: 0
-                },
-                {
-                  _id: "5a422b3a1b54a676234d17f9",
-                  title: "Canonical string reduction",
-                  author: "Edsger W. Dijkstra",
-                  url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-                  likes: 12,
-                  __v: 0
-                },
-        ];
+        const blogs =  sample_blogs_3
         const result = listHelper.totalLikes(blogs);
         assert.strictEqual(result, 24)
     });
@@ -69,5 +69,22 @@ describe('totalLikes', () => {
         ];
         const result = listHelper.totalLikes(blogs);
         assert.strictEqual(result, 10)
+    });
+});
+
+
+describe('favoriteBlog', () => {
+
+    test('only one blog is provided', () => {
+        const blogs = [sample_blogs_3[0]];
+        const result = listHelper.favoriteBlog(blogs);
+        console.log(result)
+        assert.deepStrictEqual(result, { title: 'React patterns', author: 'Michael Chan', likes: 7 });
+    });
+
+    test('multiple blogs provided', () => {
+        const blogs = sample_blogs_3
+        const result = listHelper.favoriteBlog(blogs);
+        assert.deepStrictEqual(result, { title: 'Canonical string reduction', author: 'Edsger W. Dijkstra', likes: 12 });
     });
 });
