9eb837fa9650acf8f2a07394035191cbfdc5d2ea
harinda05 <harindaburndown@gmail.com>
2025-01-04 14:14:21 +0200

    bloglist change 4.4

--- part-4/tests/dummy_test.js
+++ part-4/tests/dummy_test.js
@@ -19,21 +19,55 @@ describe('totalLikes', () => {
 
     test('total likes for multiple blogs', () => {
         const blogs = [
-            { id: 1, title: 'Blog 1', likes: 5 },
-            { id: 2, title: 'Blog 2', likes: 10 },
-            { id: 3, title: 'Blog 3', likes: 15 },
+                {
+                  _id: "5a422a851b54a676234d17f7",
+                  title: "React patterns",
+                  author: "Michael Chan",
+                  url: "https://reactpatterns.com/",
+                  likes: 7,
+                  __v: 0
+                },
+                {
+                  _id: "5a422aa71b54a676234d17f8",
+                  title: "Go To Statement Considered Harmful",
+                  author: "Edsger W. Dijkstra",
+                  url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+                  likes: 5,
+                  __v: 0
+                },
+                {
+                  _id: "5a422b3a1b54a676234d17f9",
+                  title: "Canonical string reduction",
+                  author: "Edsger W. Dijkstra",
+                  url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+                  likes: 12,
+                  __v: 0
+                },
         ];
         const result = listHelper.totalLikes(blogs);
-        assert.strictEqual(result, 30)
+        assert.strictEqual(result, 24)
     });
 
 
     test('sblogs with missing likes property', () => {
         const blogs = [
-            { id: 1, title: 'Blog 1' },
-            { id: 2, title: 'Blog 2', likes: 20 },
+            {
+                _id: "5a422b891b54a676234d17fa",
+                title: "First class tests",
+                author: "Robert C. Martin",
+                url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+                likes: 10,
+                __v: 0
+              },
+              {
+                _id: "5a422ba71b54a676234d17fb",
+                title: "TDD harms architecture",
+                author: "Robert C. Martin",
+                url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+                __v: 0
+              },
         ];
         const result = listHelper.totalLikes(blogs);
-        assert.strictEqual(result, 20)
+        assert.strictEqual(result, 10)
     });
 });
